2f80b63e8e6742e6e672f1fb75d933a8580c97fd
Steve Godin <steve.god@gmail.com>
2015-02-21 12:35:50 -0800

    Route act_summaries

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'home#index'
+
+  resources :act_summaries
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
