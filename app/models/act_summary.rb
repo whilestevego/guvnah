e98b345e6aa0c2d893fb1f44cb3c14b5a538aa93
Steve Godin <steve.god@gmail.com>
2015-02-21 15:42:58 -0800

    Add link methods to ActSummary and Regulation

--- app/models/act_summary.rb
+++ app/models/act_summary.rb
@@ -1,3 +1,11 @@
 class ActSummary < ActiveRecord::Base
   has_many :regulations
+
+  def link_to_previous_versions
+    link_to_html_toc.sub(/index\.html/, "PITIndex.html")
+  end
+
+  def link_to_html
+    link_to_html_toc.sub(/index\.html/, "FullText.html")
+  end
 end
--- app/models/regulation.rb
+++ app/models/regulation.rb
@@ -1,3 +1,11 @@
 class Regulation < ActiveRecord::Base
   belongs_to :act_summary
+
+  def link_to_previous_versions
+    link_to_html_toc.sub(/index\.html/, "PITIndex.html")
+  end
+
+  def link_to_html
+    link_to_html_toc.sub(/index\.html/, "FullText.html")
+  end
 end
