308c039dae4ca77ae1360dcf4e9292a9443773d0
Steve Godin <steve.god@gmail.com>
2015-02-21 20:06:34 -0800

    Migrate install contrib packages

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222030818) do
+ActiveRecord::Schema.define(version: 20150222040325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "act_summaries", force: :cascade do |t|
     t.string   "unique_id"
