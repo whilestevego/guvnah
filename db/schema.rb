0c08047ea6d3aaa01ec132e2e8fac694c95f5e89
Steve Godin <steve.god@gmail.com>
2015-02-21 19:08:35 -0800

    Migrate pg_search tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222004315) do
+ActiveRecord::Schema.define(version: 20150222030818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20150222004315) do
   end
 
   add_index "acts", ["act_summary_id"], name: "index_acts_on_act_summary_id", using: :btree
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "regulations", force: :cascade do |t|
     t.integer  "olid"
