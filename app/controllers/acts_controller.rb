ef43f1508fe3b3155934dffedd4acc542b005a2b
Steve Godin <steve.god@gmail.com>
2015-02-21 17:24:50 -0800

    Generate Acts controller

---
+++ app/controllers/acts_controller.rb
@@ -0,0 +1,7 @@
+class ActsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'acts/show'
+
+  get 'acts/index'
+
   concern :paginatable do
       get '(page/:page)', :action => :index, :on => :collection, :as => ''
   end
